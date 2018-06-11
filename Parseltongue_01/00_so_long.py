e6b4da9320f2a5433b8c121aa6ba1c59a0e971cd
Ahssimi mamadou Housseini <ahoussei@e1z1r4p30.42.us.org>
2018-06-10 18:54:25 -0700

    adding first exercises

---
+++ Parseltongue_01/00_so_long.py
@@ -0,0 +1,48 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    00_so_long.py                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ahoussei <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/06/10 17:40:29 by ahoussei          #+#    #+#              #
+#    Updated: 2018/06/10 17:40:32 by ahoussei         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+animal_plural = "monkeys"
+emotion = "perturbed"
+character_trait = "wholesomeness"
+adjective = "vivacious"
+
+print("So long and thanks for all the {}".format(animal_plural))
+print("So {} that it should come to this".format(emotion))
+print("We tried to warn you all but oh dear?\n")
+
+print("You may not share our {}".format(character_trait))
+print("Which might explain your disrespect")
+print("For all the {} wonders that".format(adjective))
+print("grow around you\n")
+
+print("So long, so long and thanks")
+print("for all the {}".format(animal_plural))
+
+animal_2 = "Donkey"
+emotion = "anxious"
+character_trait = "lonelyness"
+adjective = "smart"
+
+print("The world's about to be destroyed")
+print("There is no point getting all {}".format(emotion))
+print("Lie back and let the planet dissolve\n")
+
+print("Despite those nets of {} fleets".format(animal_2))
+print("We thought that most of you were {}".format(emotion))
+print("Especially {} tots and your".format(adjective))
+print("pregnant women\n")
+
+print("So long, so long, so long, so long, so long")
+print("So long, so long, so long, so long, so long\n")
+
+print("So long, so long and thanks")
+print("for all the {}".format(animal_plural))
